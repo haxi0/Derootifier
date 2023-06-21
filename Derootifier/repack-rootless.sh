c797636a9d6868edb8cae3300f3977a210b7426b
ささ <91324887+sasa8810@users.noreply.github.com>
2023-06-21 22:02:45 +0900

    Bruh

--- Derootifier/repack-rootless.sh
+++ Derootifier/repack-rootless.sh
@@ -90,7 +90,7 @@ find "$TEMPDIR_NEW" -type f | while read -r file; do
 done
 
 dpkg-deb -Zzstd -b "$TEMPDIR_NEW" "/var/mobile/.Derootifier/$(grep Package: "$TEMPDIR_NEW"/DEBIAN/control | cut -f2 -d ' ')"_"$(grep Version: "$TEMPDIR_NEW"/DEBIAN/control | cut -f2 -d ' ')"_"$(grep Architecture: "$TEMPDIR_NEW"/DEBIAN/control | cut -f2 -d ' ')".deb
-rm -rf "/var/mobile/.Derootifier/$(grep Package: "$TEMPDIR_NEW"/DEBIAN/control | cut -f2 -d ' ')"_"$(grep Version: "$TEMPDIR_NEW"/DEBIAN/control | cut -f2 -d ' ')"_iphoneos-arm.deb
+ls /var/mobile/.Derootifier/ |  grep -v -E '*iphoneos-arm64*' | xargs rm -r
 
 ### Real script end
 
